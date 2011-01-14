235b9d5c6667fcb03eddef4054ae3f1da34f32ba
Stephan Hoyer <stephan.hoyer@netresearch.de>
2011-01-14 16:12:51 +0100

    Extend information of events displayed in the events tab

--- app/code/community/Mage/Devtool/Block/Events.php
+++ app/code/community/Mage/Devtool/Block/Events.php
@@ -1,4 +1,13 @@
 <?php
 class Mage_Devtool_Block_Events extends Mage_Core_Block_Template
 {
+    /**
+     * Returns array of all fired events
+     * 
+     * @return ArrayObject
+     */
+    public function getEvents()
+    {
+        return Mage::getSingleton('devtool/event_collection');
+    }
 }
