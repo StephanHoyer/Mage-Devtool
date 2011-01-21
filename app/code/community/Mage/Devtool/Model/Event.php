dd0ae799713707db95fe58d808de3255070cad9a
Thomas Kappel <thomas.kappel@netresearch.de>
2011-01-21 15:23:16 +0100

    code style for models

--- app/code/community/Mage/Devtool/Model/Event.php
+++ app/code/community/Mage/Devtool/Model/Event.php
@@ -1,4 +1,43 @@
 <?php 
+/**
+ * Magento
+ *
+ * PHP version 5
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Mage
+ * @package   Mage_Devtool
+ * @author    Stephan Hoyer <stephan.hoyer@netresearch.de>
+ * @copyright 2011 Netresearch GmbH & Co.KG <http://www.netresearch.de/>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/leistungen/magento-ecommerce.html
+ */
+
+/**
+ * Mage_Devtool_Model_Event
+ *
+ * @category  Mage
+ * @package   Mage_Devtool
+ * @author    Stephan Hoyer <stephan.hoyer@netresearch.de>
+ * @copyright 2011 Netresearch GmbH & Co.KG <http://www.netresearch.de/>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/leistungen/magento-ecommerce.html
+ */
 class Mage_Devtool_Model_Event extends Mage_Core_Model_Abstract
 {
     /**
@@ -6,6 +45,13 @@ class Mage_Devtool_Model_Event extends Mage_Core_Model_Abstract
      */
     protected $_observer;
     
+    /**
+     * initialize
+     *
+     * @param Varien_Event_Observer $observer Observer
+     *
+     * @return Mage_Devtool_Model_Event
+     */
     public function init(Varien_Event_Observer $observer)
     {
         $this->_event = $observer->getEvent();
@@ -13,11 +59,21 @@ class Mage_Devtool_Model_Event extends Mage_Core_Model_Abstract
         return $this;
     }
     
+    /**
+     * get name of the event
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->_event->getName();
     }
     
+    /**
+     * get params of the event
+     *
+     * @return array
+     */
     public function getParams()
     {
         $params = array();
--- app/code/community/Mage/Devtool/Model/Observer.php
+++ app/code/community/Mage/Devtool/Model/Observer.php
@@ -1,10 +1,55 @@
 <?php
+/**
+ * Magento
+ *
+ * PHP version 5
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Mage
+ * @package   Mage_Devtool
+ * @author    Stephan Hoyer <stephan.hoyer@netresearch.de>
+ * @copyright 2011 Netresearch GmbH & Co.KG <http://www.netresearch.de/>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/leistungen/magento-ecommerce.html
+ */
 
+/**
+ * Netresearch_Productvisibility_Model_Observer
+ *
+ * @category  Mage
+ * @package   Mage_Devtool_Model_Observer
+ * @author    Stephan Hoyer <stephan.hoyer@netresearch.de>
+ * @copyright 2011 Netresearch GmbH & Co.KG <http://www.netresearch.de/>
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/leistungen/magento-ecommerce.html
+ */
 class Mage_Devtool_Model_Observer
 {
     const PARAM_NAME_SHOW_DEVTOOL = 'show-devtool';
     const PARAM_NAME_HIDE_DEVTOOL = 'hide-devtool';
     
+    /**
+     * catch all thrown events
+     *
+     * @param Varien_Event_Observer $observer Observer
+     *
+     * @return void
+     */
     public function attachToAllEvents($observer)
     {
         if (false == Mage::helper('devtool')->isDevtoolVisible()) {
@@ -19,7 +64,9 @@ class Mage_Devtool_Model_Observer
      * Change config that logEvent method is called on every event 
      * of given areathat is dispached
      *
-     * @var String $area can be one of global, frontend, adminhtml
+     * @param string $area Can be one of global, frontend, adminhtml
+     *
+     * @return void
      **/ 
     protected function attachToEvents($area)
     {
@@ -30,6 +77,13 @@ class Mage_Devtool_Model_Observer
         }
     }
 
+    /**
+     * log event
+     *
+     * @param Varien_Event_Observer $observer Observer
+     *
+     * @return void
+     */
     public function logEvent(Varien_Event_Observer $observer)
     {
         if (false == Mage::helper('devtool')->isDevtoolVisible()) {
@@ -39,6 +93,13 @@ class Mage_Devtool_Model_Observer
         Mage::getSingleton('devtool/event_collection')->add($event);
     }
     
+    /**
+     * replace placeholder by event tab of devtool
+     *
+     * @param Varien_Event_Observer $observer Observer
+     *
+     * @return void
+     */
     public function attachEventHtml($observer)
     {
         if (false == Mage::helper('devtool')->isDevtoolVisible()) {
@@ -50,15 +111,21 @@ class Mage_Devtool_Model_Observer
             'events',
             array('template' => 'devtool/events.phtml')
         )->toHtml();
-        $response->setBody(str_replace(
-            Mage_Devtool_Block_Events_Placeholder::EVENTS_HTML_PLACEHOLDER,
-            $events,
-            $response->getBody()
-        ));
+        $response->setBody(
+            str_replace(
+                Mage_Devtool_Block_Events_Placeholder::EVENTS_HTML_PLACEHOLDER,
+                $events,
+                $response->getBody()
+            )
+        );
     }
     
     /**
      * Checks for devtool related get-parameteters 
+     *
+     * @param Varien_Event_Observer $observer Observer
+     *
+     * @return void
      **/
     public function checkParams($observer)
     {
@@ -66,10 +133,10 @@ class Mage_Devtool_Model_Observer
             ->getControllerAction()
             ->getRequest()
             ->getParams();
-        if(array_key_exists(self::PARAM_NAME_SHOW_DEVTOOL, $params)) {
+        if (array_key_exists(self::PARAM_NAME_SHOW_DEVTOOL, $params)) {
             Mage::getSingleton('core/session')->setShowDevtool(true);
         }
-        if(array_key_exists(self::PARAM_NAME_HIDE_DEVTOOL, $params)) {
+        if (array_key_exists(self::PARAM_NAME_HIDE_DEVTOOL, $params)) {
             Mage::getSingleton('core/session')->setShowDevtool(false);
         }
     }
